8c67c4a295485c578e8213e13ea7edcc9a397c04
PrafullaS <112618564+PrafullaS@users.noreply.github.com>
2024-02-25 12:25:48 +0530

    Update start_container.sh

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,9 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+# echo
+docker pull prafulla97/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+# echo
+docker tun -d -p 5000:5000 prafulla97/simple-python-flask-app:latest
